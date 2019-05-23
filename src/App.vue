f9cb99d5ad33b79676b321b06d2c41341f7915b4
Luke Carbis <luke@carb.is>
2019-05-23 16:20:02 +1000

    Remove custom Router and use ViewRouter instead

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,34 @@
     <h1>Connect 4</h1>
     <nav>
       <ul>
-        <li><a href="/">Results</a></li>
-        <li><a href="/new/match">New Match</a></li>
-        <li><a href="/new/player">New Player</a></li>
+        <li><router-link to="/">Results</router-link></li>
+        <li><router-link to="/new/match">New Match</router-link></li>
+        <li><router-link to="/new/player">New Player</router-link></li>
       </ul>
     </nav>
-    <RouterView/>
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
-import RouterView from './components/RouterView'
+import ResultsTable from './components/ResultsTable'
+import AddMatch from './components/AddMatch'
+import AddPlayer from './components/AddPlayer'
+import VueRouter from 'vue-router'
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: ResultsTable },
+    { path: '/new/match', component: AddMatch },
+    { path: '/new/player', component: AddPlayer },
+  ]
+})
 
 export default {
   name: 'App',
-  components: {
-    RouterView
-  }
+  router
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import { firestorePlugin } from 'vuefire'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(firestorePlugin)
 
 new Vue({
